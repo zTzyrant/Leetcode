22d74731f81bb2b338f699c96b6ca9cb33cdd4aa
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2023-06-03 09:45:27 +0800

    Two Sum

---
+++ ts/Two Sum/TwoSum.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/two-sum/description/
+
+function twoSum(nums: number[], target: number): number[] {
+    let result: any[] = []
+    for (let x = 0; x < nums.length; x++) {
+        for (let k = 0; k < nums.length; k++) {
+            if(x !== k){
+                if(nums[x] + nums[k] === target){
+                    result.push(x)
+                    result.push(k)
+                    x = nums.length
+                    k = nums.length
+                }
+            }
+        }
+    }
+    return result
+};
+
+console.log(twoSum([2,7,11,15], 9));
